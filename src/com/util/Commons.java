90d0a0296cb341bf3b23577de742e5a473915435
thesomeexp <someexp@riseup.net>
2020-01-06 17:09:24 +0800

    register fail

--- src/com/util/Commons.java
+++ src/com/util/Commons.java
@@ -4,6 +4,7 @@ public class Commons {
     public static String UN_LOGIN = "未登录";
     public static String LOGIN_FAIL = "登录失败";
     public static String PARAMETER_IS_EMPTY = "参数为空";
+    public static String REGISTER_FAIL = "注册失败";
     
     
     
