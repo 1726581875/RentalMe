5b4cf5307666ae636c5bda9d7760759004c53104
thesomeexp <someexp@riseup.net>
2020-01-06 10:55:56 +0800

    change user_state

---
+++ src/com/util/CommonsState.java
@@ -0,0 +1,16 @@
+package com.util;
+
+import com.pojo.User;
+
+public class CommonsState {
+//    user role
+    public static int USER_ROLE_USER = 0;
+    public static int USER_ROLE_ADMIN = 1;
+//    user state
+    public static int USER_STATE_NORMAL = 0;
+    public static int USER_STATE_LOCK = 1;
+    public static int USER_STATE_UNPAY = 2;
+    public static int USER_STATE_UNCERTIFIED = 3;
+    
+    
+}
