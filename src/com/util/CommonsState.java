2a05340db2f856f1f9a8c7ee30df1aa9ee46fae1
thesomeexp <someexp@riseup.net>
2020-01-06 11:41:04 +0800

    add commons

--- src/com/util/CommonsState.java
+++ src/com/util/CommonsState.java
@@ -1,5 +1,6 @@
 package com.util;
 
+import com.pojo.Item;
 import com.pojo.User;
 
 public class CommonsState {
@@ -11,6 +12,28 @@ public class CommonsState {
     public static int USER_STATE_LOCK = 1;
     public static int USER_STATE_UNPAY = 2;
     public static int USER_STATE_UNCERTIFIED = 3;
+    
+//    item state
+    public static int ITEM_STATE_ENABLE = 1;
+    public static int ITEM_STATE_DISABLE = 0;
+//    review review
+    public static int REVIEW_STATE_PRAISE = 1;
+    public static int REVIEW_STATE_BAD = 0;
+//    order status
+    public static String BUYER_UNPAYMENT = "买家未付款";
+    public static String BUYER_CANCELLED = "买家已取消";
+    public static String SALLER_CANCELLED = "卖家已取消";
+    public static String SALLER_UNLOAN = "卖家未借出";
+    public static String BUYER_UNRETURNED = "买家未还";
+    public static String UNREVIEW = "未评价";
+    public static String BUYER_REVIEW = "买家已评价";
+    public static String SALLER_REVIEW = "卖家已评价";
+    public static String BUYER_UNPAY_BALANCE = "买家未付尾款";
+    public static String BOTH_REVIEW = "双方已评价";
+    
+    
+    
+    
     
     
 }
