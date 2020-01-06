324958331067c19d5417d11e7e85b79f3fbc3add
thesomeexp <someexp@riseup.net>
2020-01-06 11:15:03 +0800

    未登录

--- src/com/util/Commons.java
+++ src/com/util/Commons.java
@@ -1,6 +1,7 @@
 package com.util;
 
 public class Commons {
+    public static String UN_LOGIN = "未登录";
     public static String LOGIN_FAIL = "登录失败";
     public static String PARAMETER_IS_EMPTY = "参数为空";
     
