48e8aa103385ad80b2d140c2078a23c3a8ffcead
thesomeexp <someexp@riseup.net>
2020-01-09 09:42:15 +0800

    tx

--- src/com/service/IordersService.java
+++ src/com/service/IordersService.java
@@ -3,10 +3,12 @@ package com.service;
 import java.util.List;
 
 import com.pojo.Orders;
+import com.pojo.User;
 
 public interface IordersService {
 	Orders get(int id);
 	List<Orders> list(Orders id);	
 	int insert(Orders orders);
 	int update(Orders orders);
+	void txUserPayOrders(User user, Orders orders);
 }
--- src/com/util/Commons.java
+++ src/com/util/Commons.java
@@ -1,6 +1,8 @@
 package com.util;
 
 public class Commons {
+    public static final Object CREATE_ORDER_FAIL = "创建订单错误";
+
     public static String SERVER_URI = "http://localhost:8080/RentalMe";
     
     public static String UN_LOGIN = "未登录";
@@ -14,6 +16,6 @@ public class Commons {
     public static String NOT_FOUND = "未找到";
     public static Object INSERT_FAIL = "插入失败";
     public static Object ORDER_ERROR = "不能自己下单自己";
-    
+    public static Object INSUFFICIENT_BALANCE = "余额不足";
     
 }
